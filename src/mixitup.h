95f32f1170573a2e386025e85402519af9d2d58e
hikir1 <hikirone@gmail.com>
2020-03-06 17:24:14 -0500

    added header guard to mixitup

--- src/mixitup.h
+++ src/mixitup.h
@@ -1,3 +1,6 @@
+#ifndef MIXITUP_H
+#define MIXITUP_H
+
 #include<stdlib.h>
 
 // permute tokens
@@ -7,3 +10,5 @@ inline void permute(const char * const toks, size_t numtoks, char * const res);
 // make list of tokens
 // select 'listlen' number of elements from 'toks' at random and insert them to 'list'
 inline void mklist(const char * const toks, size_t numtoks, char * const list, size_t listlen);
+
+#endif
